e222899e60a28ed885b6734e652459d9919f5b43
Matheus Pereira <66024538+lopesmatheus7@users.noreply.github.com>
2023-05-07 19:47:32 -0300

    Reformulação tela cadastro

--- login.js
+++ login.js
@@ -1,14 +1,36 @@
 
+var email = document.querySelector('email')
+
+function cadastrar(){
+    
+
+    if (localStorage.cont) {
+        localStorage.cont = Number(localStorage.cont)+1
+    } else {
+        localStorage.cont = 1;
+    }
+    }
+    cadastri = document.getElementById('email').value;
+    localStorage.setItem("cad_"+localStorage.cont,cad);
 function logar(){
 
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
     
-    if(login == "admin" && senha == "admin"){
-        alert('Sucesso');
-        location.href = "index.html";
+    if(login == "email"){location.href = "index.html";
     }
-    else{alert('Usuario ou senha incorretos');
+    else{document.getElementById('ErrorRecorder').style.display = "inline";
     }
+            if(senha == "1234"){
+                
+            }
+            else{document.getElementById("Errorsenha").style.display = "inline";}
+}
+function error(){
+    var login = document.getElementById('errorEmail').value;
+
+    if (!Email) {
+        document.getElementById('ErrorRecorder').style.display = "block"
     }
     
+}
