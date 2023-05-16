c9f2e75aea9e0ccc8414e3fd34a9fb2f7557771e
Matheus Pereira <66024538+lopesmatheus7@users.noreply.github.com>
2023-05-16 18:49:49 -0300

    Atuazação login

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,11 @@
 function saveDataToLocalStorage() {
     const tarefa = document.querySelector("#tarefa").value;
+    const pass = document.querySelector("#pass").value;
+
+
+
     localStorage.setItem("email", tarefa);
     console.log(tarefa);
+    localStorage.setItem("senha", pass);
+    console.log(pass);
   }
--- login.js
+++ login.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 
 var email = document.querySelector('email')
 
@@ -14,41 +13,12 @@ function cadastrar(){
     }
     cadastri = document.getElementById('email').value;
     localStorage.setItem("cad_"+localStorage.cont,cad);
->>>>>>> 06a9a4c2506a3e80be6cec3d42e8c65671163f38
 function logar(){
 
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
     
-<<<<<<< HEAD
-    if(login == localStorage.getItem('email'),senha == ('1234')){location.href = "index.html";
-    }
+
+    if (login == localStorage.getItem("email") &&  senha == localStorage.getItem("senha")){location.href ='index.html'}
     else{document.getElementById('ErrorRecorder').style.display = "inline", document.getElementById('Errorsenha').style.display = 'inline';
-=======
-    if(login == "email"){location.href = "index.html";
-    }
-    else{document.getElementById('ErrorRecorder').style.display = "inline";
->>>>>>> 06a9a4c2506a3e80be6cec3d42e8c65671163f38
-    }
-            if(senha == "1234"){
-                
-            }
-            else{document.getElementById("Errorsenha").style.display = "inline";}
-}
-function error(){
-    var login = document.getElementById('errorEmail').value;
-
-    if (!Email) {
-        document.getElementById('ErrorRecorder').style.display = "block"
-    }
-<<<<<<< HEAD
-function error(){
-    var login = document.getElementById('errorEmail').value;
-
-    if (!Email) {
-        document.getElementById('ErrorRecorder').style.display = "block"
-    }
-=======
->>>>>>> 06a9a4c2506a3e80be6cec3d42e8c65671163f38
-    
-}
+    }}
